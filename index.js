95ca31f02a90a2c185a203c2a9aa281e639ba801
Jon Kaplon <jon.kaplon1@gmail.com>
2022-02-02 10:24:20 -0800

    commit tree 9c45ee551652a14fe470de1991f8bb3bc9831bf5

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
+
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() + 1)
+
+const num3 = 22;
+const num4 = 18;
+
+const mod = num3 % num4;
+
+const max = Math.max(1, 5, 20, 5)
